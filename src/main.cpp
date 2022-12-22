47d7105250b6a203f156c1260a2b3c9933f115f3
Richard Xiao <richard.xiao13@gmail.com>
2022-12-22 16:58:47 -0600

    Add numThreads as optional command argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <unistd.h>
 
 #include "vector3d.h"
 #include "parser.h"
@@ -9,22 +10,33 @@ using namespace std;
 
 int main(int argc, char **argv) {
   Scene *scene = nullptr;
-  if (argc > 2) {
-    cerr << "usage: ./raytracer [filename]" << endl;
+  if (argc > 3) {
+    cerr << "usage: " << argv[0] << "[-t numThreads] filepath" << endl;
     return 1;
-  } else if (argc == 2) {
-    ifstream infile(argv[1]);
-    if (infile) {
-      scene = readDataFromStream(infile);
-    } else {
-      cerr << "Couldn't open file " << argv[1] << endl;
-      return 1;
-    }
-  } else {
-    scene = readDataFromStream(cin);
   }
 
-  PNG *renderedScene = scene->render();
+  int opt;
+  int numThreads = 4;
+  while ((opt = getopt(argc, argv, "t:")) != -1) {
+    switch (opt) {
+      case 't':
+        numThreads = atoi(optarg);
+        break;
+      default:
+        cerr << "usage: " << argv[0] << "[-t numThreads] filepath" << endl;
+        return -1;
+    }
+  }
+
+  ifstream infile(argv[2]);
+  if (infile) {
+    scene = readDataFromStream(infile);
+  } else {
+    cerr << "Couldn't open file " << argv[1] << endl;
+    return 1;
+  }
+
+  PNG *renderedScene = scene->render(numThreads);
   renderedScene->saveToFile(scene->filename());
 
   delete renderedScene;
