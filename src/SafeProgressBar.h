97e4b0e4d85ca7f5039be7337e539d7039bfbe98
Richard Xiao <richard.xiao13@gmail.com>
2023-01-06 17:14:21 -0600

    Fix progress bar updating

--- src/SafeProgressBar.h
+++ src/SafeProgressBar.h
@@ -6,17 +6,18 @@ void displayRenderProgress(float progress, int barWidth=70);
 
 class SafeProgressBar {
 public:
-  SafeProgressBar() : counter(0), barWidth(70), total(100), update(1.0) {
+  SafeProgressBar() : counter(0), barWidth(70), total(100), update(1.0), inc(1.0) {
     displayRenderProgress(0.0f, barWidth);
   };
-  SafeProgressBar(int width, int total, int update) : counter(0), barWidth(width), total(total), update(update) {
+  SafeProgressBar(int width, int total, int update) : counter(0), barWidth(width), total(total), update(update), inc(update) {
     displayRenderProgress(0.0f, barWidth);
   };
   void increment(int amount=1) {
     std::lock_guard<std::mutex> lock(m);
     counter += amount;
-    if (counter == total || counter % update == 0) {
+    if (counter == total || counter >= update) {
       displayRenderProgress(static_cast<float>(counter) / total, barWidth);
+      update += inc;
     }
     if (counter == total) {
       std::cout << std::endl;
@@ -28,4 +29,5 @@ private:
   int barWidth;
   int total;
   int update;
+  int inc;
 };
